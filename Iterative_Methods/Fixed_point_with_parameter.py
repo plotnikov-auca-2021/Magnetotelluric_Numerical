ff177c20d23f67f8bdfaf3c51297e901439eb4fd
Nikita Plotnikov <plotnikov_n@auca.kg>
2023-06-21 14:19:53 +0600

    Upd Fixed_point(aka Newton's) add system of equation

--- Iterative_Methods/Fixed_point_with_parameter.py
+++ Iterative_Methods/Fixed_point_with_parameter.py
@@ -1,26 +1,31 @@
+import numpy as np
+
+
 def g(x):
-    return x * (x - 1)
+    f1 = x[0] ** 2 - x[1] - 1
+    f2 = x[0] - x[1] ** 2 + 1
+    return np.array([f1, f2])
 
 
 def d(x):
-    return 2 * x - 1
+    return np.array([[2 * x[0], -1], [1, -2 * x[1]]])
 
 
-def fixed_point_iteration(x0, tol=0.01, max_iters=10000):
+def fixed_point_iteration(x0, tol=0.001, max_iters=1000):
     i = 0
     while i < max_iters:
-        x1 = x0 - (1 / d(x0)) * g(x0)
-        if abs(x1 - x0) < tol:
+        x1 = x0 - np.linalg.inv(d(x0)) @ g(x0)
+        if np.linalg.norm(x1 - x0) < tol:
             return x1
         x0 = x1
         i += 1
     return None
 
 
-x_initial_first = 100
+x_initial_first = np.array([100, 100])
 first_root = fixed_point_iteration(x_initial_first)
 
-if first_root:
+if first_root is not None:
     print(f"Root: {first_root}")
 else:
     print("Root cannot be found within the maximum number of iterations.")
