b94a67e920f586b61a550be31481c7d13747cabd
Nikita Plotnikov <plotnikov_n@auca.kg>
2023-06-21 13:46:15 +0600

    Finish Fixed_point with parameter

---
+++ Iterative_Methods/Fixed_point_with_parameter.py
@@ -0,0 +1,26 @@
+def g(x):
+    return x * (x - 1)
+
+
+def d(x):
+    return 2 * x - 1
+
+
+def fixed_point_iteration(x0, tol=0.01, max_iters=10000):
+    i = 0
+    while i < max_iters:
+        x1 = x0 - (1 / d(x0)) * g(x0)
+        if abs(x1 - x0) < tol:
+            return x1
+        x0 = x1
+        i += 1
+    return None
+
+
+x_initial_first = 100
+first_root = fixed_point_iteration(x_initial_first)
+
+if first_root:
+    print(f"Root: {first_root}")
+else:
+    print("Root cannot be found within the maximum number of iterations.")
